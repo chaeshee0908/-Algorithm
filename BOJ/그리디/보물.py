842bcd1a802c34d68159ab225cbcc441de5da18f
chaeshee0908 <chaeshee0908@naver.com>
2023-02-16 23:52:25 +0900

    2/16

---
+++ BOJ/그리디/보물.py
@@ -0,0 +1,14 @@
+# 실버4 1026번
+# https://www.acmicpc.net/problem/1026
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+result = 0
+for i in range(N):
+    result += A[i] * B[i]
+
+print(result)
